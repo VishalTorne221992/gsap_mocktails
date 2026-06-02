ea8d6795c90ba41c2b542894f52e6f062a24ead5
Vishal Torne <vt221992@gmail.com>
2026-06-02 20:42:51 +0530

    corrected gsap/all import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollTrigger, SplitText } from 'gsap/All'
+import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
